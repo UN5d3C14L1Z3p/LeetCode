329b83f7d61e749c2f821af164aa350f7c2f95e2
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-10-08 12:18:37 +0800

    Add initial passing solution.

---
+++ two_sum/src/lib.rs
@@ -0,0 +1,53 @@
+// Easy
+//
+// Given an array of integers nums and an integer target, return indices of the two numbers such
+// that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same
+// element twice.
+// You can return the answer in any order.
+//
+// Example 1:
+//
+// Input: nums = [2, 7, 11, 15], target = 9
+// Output: [0, 1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+//
+// Example 2:
+//
+// Input: nums = [3, 2, 4], target = 6
+// Output: [1, 2]
+//
+// Example 3:
+//
+// Input: nums = [3, 3], target = 6
+// Output: [0, 1]
+//
+// Constraints:
+//
+// * 2 <= nums.length <= 10^4
+// * -10^9 <= nums[1[ <= 10^9
+// * -10^9 <= target <= 10^9
+// * Only one valid answer exists.
+//
+// Follow-up: Can yo ucome up with an algorithm that is less than O(n^2) time complexity?
+//
+struct Solution {}
+
+impl Solution {
+    pub fn two_sum(_nums: Vec<i32>, _target: i32) -> Vec<i32> {
+        [0, 1].to_vec()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_two_sum() {
+        assert_eq!(
+            Solution::two_sum([2, 7, 11, 15].to_vec(), 9),
+            [0, 1].to_vec()
+        );
+    }
+}
