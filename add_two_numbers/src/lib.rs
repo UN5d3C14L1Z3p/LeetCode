fd306cc072c779a6c9627f44e85a6a7d61bc1dab
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-10-10 19:29:04 +0800

    Create initial passing test.

--- add_two_numbers/src/lib.rs
+++ add_two_numbers/src/lib.rs
@@ -1,14 +1,66 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Easy
+//
+// Add Two Numberrs
+//
+// You are given two non-empty linked lists representing two non-negative numberse. The digits are
+// stored in reverse order, and each of their nodes contains a single digit. Add the two numbers
+// and return the sum as a linked list.
+//
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//
+// Example 1:
+// (2) -> (4) -> (3)
+// (5) -> (6) -> (4)
+// (7) -> (0) -> (8)
+//
+// Input: l1 = [2, 4, 3], l2 = [5, 6, 4]
+// Output: [7, 0, 8]
+// Explanation: 342 + 465 = 807.
+//
+// Example 2:
+// Input: l1 = [0], l2 = [0]
+// Output: [0]
+//
+// Example 3:
+// Input: l1 = [9, 9, 9, 9, 9, 9, 9], l2 = [9, 9, 9, 9]
+// Output: [8, 9, 9, 9, 0, 0, 0, 1]
+//
+// Constraints
+// * The number of nodes in each linked list is in the range [1, 100].
+// * 0 <= Node.val <= 9
+// * It is guaranteed that the list represents a number that does not have leading zeroes.
+//
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn add_two_numbers(
+        _l1: Option<Box<ListNode>>,
+        _l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let b = Box::new(ListNode::new(1));
+        Some(b)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn test_add_two_numbers() {}
 }
