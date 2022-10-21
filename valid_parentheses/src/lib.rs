d8d6ded4922a791ed438b278e40c3a9764d84968
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-10-21 18:45:35 +0800

    Create boilerplate code.

---
+++ valid_parentheses/src/lib.rs
@@ -0,0 +1,52 @@
+// Valid Parentheses
+//
+// Easy
+//
+// Given a string s containing just the characters '(', ')', '{', '}', '[', determine if the input
+// string is valid.
+//
+// An input string is valid if:
+//
+// Open brackets must be closed by the same type of brackets.
+//
+// Open brackets must be closed in the correct order.
+//
+// Every close bracket has a corresponding open bracket of the same type.
+//
+// Example 1:
+//   Input: s = "()"
+//   Output: true
+//
+// Example 2:
+//
+//   Input: s = "()[]{}"
+//   Output: true
+//
+// Example 3:
+//   Input: s = "(]"
+//   Output: false
+//
+// Constraints:
+//
+// * 1 <= s.length <= 10^4
+// * s consists of parentheses only '()[]{}'.
+//
+struct Solution {}
+
+impl Solution {
+    pub fn is_valid(s: String) -> bool {
+        s.is_empty()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid() {
+        assert_eq!(Solution::is_valid("()".to_string()), true);
+        assert_eq!(Solution::is_valid("()[]{}".to_string()), true);
+        assert_eq!(Solution::is_valid("(]".to_string()), false);
+    }
+}
