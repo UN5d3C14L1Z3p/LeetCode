c00fd34baa2208b0a239ef4e9201f525ff9b9198
un5d3c14l1z3p <un5d3c14l1z3p.proton.me>
2022-11-09 20:33:09 +0800

    Create template code.

---
+++ length_of_last_word/src/lib.rs
@@ -0,0 +1,54 @@
+/// Length of Last Word
+///
+/// Easy
+///
+/// Given a string s consisting of words and spaces, return the length of the last word in the
+/// string.
+///
+/// A word is a maximal substring consisting of non-space characters only.
+///
+/// Example !:
+///   Input: s = "Hello World"
+///   Output: 5
+///   Explanation: The last word is "World" with length 5.
+///
+/// Example 2:
+///   Input: s = "   fly me   to    the moon "
+///   Output: 4
+///   Explanation: The last word is "moon" with length 4.
+///
+/// Example 3:
+///   Input: s = "luffy is still joyboy"
+///   Output: 6
+///   Explanation: The last word is "joyboy" with length 6.
+///
+/// Constraints:
+///
+/// * 1 <= s.length <= 10^4
+/// * s consists of only English letters and spaces ' '.
+/// * There will be at least one word in s.
+///
+struct Solution {}
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.len() as i32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_length_of_last_word() {
+        assert_eq!(Solution::length_of_last_word("Hello World".to_string()), 5);
+        assert_eq!(
+            Solution::length_of_last_word("    fly me  to  the moon ".to_string()),
+            4
+        );
+        assert_eq!(
+            Solution::length_of_last_word("luffy is still joyboy".to_string()),
+            6
+        );
+    }
+}
